20d32e31ca0e5712aaf1842e8a737d01a2502cdf
amritagithub <amrita.jha88@gmail.com>
2026-05-03 20:58:09 +0530

    changes in headless mode

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  //forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
@@ -31,7 +31,7 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 browserName : 'chromium',
-headless : false
+headless : true
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     
   },
